898e478fea0aefc030853b220145262bd7dca8c3
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-06-26 18:29:08 +0200

    10. Spring Boot REST API Validation

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -2,10 +2,22 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "Name should not be null or empty.")
     private String name;
+
+    @NotEmpty(message = "Email should not be null or empty.")
+    @Email(message = "Email should have valid email form.")
     private String email;
+
+    @NotEmpty
+    @Min(10)
     private String body;
 }
--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -2,13 +2,29 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class PostDto {
     private long id;
+    // title should be not null or empty
+    // title should have min two characters
+    @NotEmpty
+    @Min(2)
     private String title;
+
+    // description should be not null or empty
+    // description should have min 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have min 10 characters.")
     private String description;
+
+    // description should be not null or empty
+    @NotEmpty
     private String content;
+
     private Set<CommentDto> comments;
 }
