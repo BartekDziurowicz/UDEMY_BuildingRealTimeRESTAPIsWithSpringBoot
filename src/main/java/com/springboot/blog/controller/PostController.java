bd6c63035b0d8f09a074f0c55d901f7f918690e1
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-06-28 16:24:58 +0200

    14.95. Versioning through content ngotiation

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -41,12 +41,12 @@ public class PostController {
     }
 
     // get post by id
-    @GetMapping(value = "/api/posts/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/api/posts/{id}", produces = "application/vnd.javaguides.v1+json")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name="id") long id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
-    @GetMapping(value = "/api/posts/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/api/posts/{id}", produces = "application/vnd.javaguides.v2+json")
     public ResponseEntity<PostDtoV2> getPostByIdV2(@PathVariable(name="id") long id){
         PostDto postDto = postService.getPostById(id);
         PostDtoV2 postDtoV2 = new PostDtoV2();
